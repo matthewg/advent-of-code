2dabcf67358f45c3595cb144d82a992963cea7cc
Matthew Sachs <matthewg@zevils.com>
2022-12-10 15:53:37 -0800

    day 2

--- 2022/10/10.go
+++ 2022/10/10.go
@@ -26,23 +26,43 @@ func ParseInstruction(s string) Instruction {
 	return Instruction{delay: 0, addToX: 0}
 }
 
+func Abs(i int) int {
+	if i < 0 {
+		return i * -1
+	} else {
+		return i
+	}
+}
+
 func main() {
 	regX := 1
 	instructions := aocutils.Lines(os.Args[1], ParseInstruction)
 	var pipeline []Instruction
 	sigSum := 0
-	for cycle := 1; cycle <= 220; cycle++ {
-		fmt.Printf("At start of cycle %v, X: %v\n", cycle, regX)
+	for cycle := 1; cycle <= 241; cycle++ {
+		//fmt.Printf("At start of cycle %v, X: %v\n", cycle, regX)
 		if len(pipeline) == 0 {
 			if len(instructions) > 0 {
 				pipeline = append(pipeline, instructions[0])
 				instructions = instructions[1:]
 			}
 		}
+
+		// Day 2
+		scanPos := (cycle - 1) % 40
+		if Abs(scanPos-regX) <= 1 {
+			fmt.Print("#")
+		} else {
+			fmt.Print(".")
+		}
+		if cycle > 20 && cycle%40 == 0 {
+			fmt.Print("\n")
+		}
+
 		if (cycle+20)%40 == 0 {
 			sigStrength := cycle * regX
 			sigSum += sigStrength
-			fmt.Printf("Signal strength at cycle %v: %v\n", cycle, sigStrength)
+			//fmt.Printf("Signal strength at cycle %v: %v\n", cycle, sigStrength)
 		}
 		if len(pipeline) > 0 {
 			i := &pipeline[0]
@@ -52,7 +72,7 @@ func main() {
 			} else {
 				i.delay--
 			}
-			fmt.Printf("After executing instruction %v, X: %v\n", *i, regX)
+			//fmt.Printf("After executing instruction %v, X: %v\n", *i, regX)
 		}
 	}
 	fmt.Printf("Sum of signal strengths: %v\n", sigSum)
