123596b1658cd75e8b119de6db760cbf5033ec71
Matthew Sachs <matthewg@zevils.com>
2022-12-14 20:42:16 -0800

    day 14

--- 2022/14/14.go
+++ 2022/14/14.go
@@ -64,15 +64,84 @@ func ParseRock(s string) []RockLine {
 	return ret
 }
 
+func ReleaseSand(rockuppied map[Pos]bool, maxRockY int, useFloor bool) int {
+	occupied := make(map[Pos]bool)
+	for k, v := range rockuppied {
+		occupied[k] = v
+	}
+
+	floor := maxRockY + 2
+	isOccupied := func(p Pos) bool {
+		return occupied[p] || (useFloor && p.Y >= floor)
+	}
+	if useFloor {
+		maxRockY += 2
+	}
+
+	minX := 0
+	maxX := 500
+
+	sandRested := 0
+	origin := Pos{X: 500, Y: 0}
+	for {
+		sandCanRest := false
+		//fmt.Printf("Releasing sand grain %v...\n", sandRested)
+		for sand := origin; sand.Y <= maxRockY && !isOccupied(origin); {
+			//fmt.Printf("  Sand is at %v\n", sand)
+			next := Pos{X: sand.X, Y: sand.Y + 1}
+			if isOccupied(next) {
+				next = Pos{X: sand.X - 1, Y: sand.Y + 1}
+				if isOccupied(next) {
+					next = Pos{X: sand.X + 1, Y: sand.Y + 1}
+					if isOccupied(next) {
+						occupied[sand] = true
+						if sand.X > maxX {
+							maxX = sand.X
+						}
+						if sand.X < minX {
+							minX = sand.X
+						}
+						sandRested++
+						sandCanRest = true
+						break
+					}
+				}
+			}
+			sand = next
+		}
+		if !sandCanRest {
+			break
+		}
+	}
+
+	for y := 0; y <= maxRockY; y++ {
+		for x := minX; x <= maxX; x++ {
+			p := Pos{X: x, Y: y}
+			if rockuppied[p] || (useFloor && y == maxRockY) {
+				fmt.Printf("#")
+			} else if occupied[p] {
+				fmt.Printf("o")
+			} else {
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Printf("\n")
+	}
+	return sandRested
+}
+
 func main() {
 	lines := aocutils.Lines(os.Args[1], ParseRock)
-	occupied := make(map[Pos]bool)
+	occupied1 := make(map[Pos]bool)
+	occupied2 := make(map[Pos]bool)
 
+	fmt.Printf("Marking rocks\n")
 	maxRockY := 0
 	for _, l := range lines {
 		for _, line := range l {
 			for _, lp := range line.AllPos() {
-				occupied[lp] = true
+				occupied1[lp] = true
+				occupied2[lp] = true
 				if lp.Y > maxRockY {
 					maxRockY = lp.Y
 				}
@@ -80,26 +149,11 @@ func main() {
 		}
 	}
 
-	sandRested := 0
-	for {
-		sandCanRest := false
-		for sand := (Pos{X: 500, Y: 0}); sand.Y <= maxRockY; sand.Y++ {
-			next := Pos{X: sand.X, Y: sand.Y + 1}
-			if occupied[next] {
-				next = Pos{X: sand.X - 1, Y: sand.Y + 1}
-				if occupied[next] {
-					next = Pos{X: sand.X + 1, Y: sand.Y + 1}
-					if occupied[next] {
-						sandRested++
-						sandCanRest = true
-						break
-					}
-				}
-			}
-		}
-		if !sandCanRest {
-			break
-		}
-	}
-	fmt.Printf("Sand grains rested: %v\n", sandRested)
+	// Part 1
+	sandRested := ReleaseSand(occupied1, maxRockY, false)
+	fmt.Printf("Sand grains rested, part 1: %v\n", sandRested)
+
+	// Part 2
+	sandRested = ReleaseSand(occupied2, maxRockY, true)
+	fmt.Printf("Sand grains rested, part 2: %v\n", sandRested)
 }
