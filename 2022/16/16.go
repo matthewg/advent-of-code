9f02e7fa1491d1d6620c29e9ffb959befb5d6865
Matthew Sachs <matthewg@zevils.com>
2022-12-22 23:34:39 -0800

    part 1 day 16

--- 2022/16/16.go
+++ 2022/16/16.go
@@ -1,18 +1,3 @@
-// valves: flow rate (gpm, int), open/closed
-// ...15 valves, flow rate is small int, many 0
-// ...each valve has tunnels to 1-4 other valves
-// Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-// time limit is 30 minutes
-// valves start closed
-// start w/ elephants in AA
-// takes 1 minute to open a valve, 1 minute to follow a tunnel
-// trying to maximize released pressure *over time*, so multiple pressure * time remaining
-// return amount of pressure released
-//
-// Something like ~1152921504606846976 possible paths... need to be clever about pruning.
-// Maybe something like 'do not backtrack unless you have opened a valve'!
-// Good start, need to get more clever...
-
 package main
 
 import (
@@ -21,6 +6,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/albertorestifo/dijkstra"
 	"github.com/matthewg/advent-of-code/golang/aocutils"
 )
 
@@ -30,115 +16,124 @@ type Valve struct {
 	Flow      int
 }
 
-func DLS(start *Valve, valveMap *map[string]*Valve, depthLimit int, valvesOpen map[string]bool, path []string, visitedSinceLastOpen map[string]bool) []string {
-	if depthLimit <= 0 {
-		return path
-	}
-
-	bestPath := path
-	bestPressure := Pressure(path, valveMap)
-	valve := (*valveMap)[start.Name]
-	valveOpen, ok := valvesOpen[valve.Name]
-	if !ok {
-		valveOpen = false
-	}
-	if valve.Flow > 0 && !valveOpen {
-		newPath := path
-		newPath = append(newPath, "open")
-		newOpen := make(map[string]bool)
-		for k, v := range valvesOpen {
-			newOpen[k] = v
-		}
-		newOpen[valve.Name] = true
-		bestPath = newPath
-		if depthLimit > 1 {
-			bestPath = DLS(valve, valveMap, depthLimit-1, newOpen, newPath, make(map[string]bool))
-			bestPressure = Pressure(path, valveMap)
-		}
-
-	}
-
-	for _, n := range valve.Neighbors {
-		if _, ok := visitedSinceLastOpen[n]; ok {
-			continue
-		}
-		newPath := path
-		newPath = append(newPath, n)
-		newVSLO := make(map[string]bool)
-		for k, v := range visitedSinceLastOpen {
-			newVSLO[k] = v
-		}
-		newVSLO[n] = true
-		newOpen := make(map[string]bool)
-		for k, v := range valvesOpen {
-			newOpen[k] = v
-		}
-		newPath = DLS((*valveMap)[n], valveMap, depthLimit-1, newOpen, newPath, newVSLO)
-		if newPressure := Pressure(newPath, valveMap); newPressure > bestPressure {
-			bestPressure = Pressure(newPath, valveMap)
-			bestPath = newPath
-		}
-	}
-	return bestPath
-}
-
-var PathsExplored int = 0
-var Cache map[string]int = make(map[string]int)
-
-func Pressure(path []string, valveMap *map[string]*Valve) int {
-	cKey := strings.Join(path, ",")
-	pressure, ok := Cache[cKey]
-	if ok {
-		return pressure
-	}
-	PathsExplored++
-	if PathsExplored%1000 == 0 {
-		fmt.Printf("Explored paths: %v\n", PathsExplored)
-	}
-
-	pressure = 0
-	gpm := 0
-	current := "AA"
-	//fmt.Printf("Calculating pressure of %v-path: %v\n", len(path), path)
-	for _, step := range path {
-		pressure += gpm
-		//fmt.Printf("Existing valves release another %v; total released is %v\n", gpm, pressure)
-		if step == "open" {
-			gpm += (*valveMap)[current].Flow
-			//fmt.Printf("After opening %v, new gpm is %v: %v\n", current, gpm, (*valveMap)[current])
-		} else {
-			current = step
-		}
-	}
-	Cache[cKey] = pressure
-	return pressure
-}
-
-func IDDFS(start *Valve, valveMap *map[string]*Valve) int {
-	valvesOpen := make(map[string]bool)
-	path := DLS(start, valveMap, 30, valvesOpen, []string{}, make(map[string]bool))
-	fmt.Printf("Best path: %v\n", path)
-	return Pressure(path, valveMap)
-}
+var ValveMap map[string]*Valve
+var ValveGraph dijkstra.Graph
+var FlowGraph dijkstra.Graph
 
 var Parser = regexp.MustCompile(`Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.*)`)
 
-func ParseValve(s string) Valve {
+func ParseValve(s string) *Valve {
 	matches := Parser.FindStringSubmatch(s)
-	return Valve{
+	v := Valve{
 		Name:      matches[1],
 		Flow:      aocutils.Atoi(matches[2]),
 		Neighbors: strings.Split(matches[3], ", "),
 	}
+	ValveMap[v.Name] = &v
+	ValveGraph[v.Name] = make(map[string]int)
+	for _, n := range v.Neighbors {
+		ValveGraph[v.Name][n] = 1
+	}
+	return &v
+}
+
+func OpenValves(pathCost, pathFlow int, path []string, pos string, openValves map[string]bool) (int, []string) {
+	bestFlow := pathFlow
+	path = append(path, pos)
+	bestPath := path
+	maxCost := 30
+	for valve, cost := range FlowGraph[pos] {
+		if cost+pathCost > maxCost || openValves[valve] {
+			continue
+		}
+
+		newOpenValves := make(map[string]bool)
+		for k, v := range openValves {
+			newOpenValves[k] = v
+		}
+		newOpenValves[valve] = true
+		newCost := pathCost
+		newFlow := pathFlow
+		//newFlow += ValveMap[valve].Flow
+		newCost += cost
+		newFlow += (maxCost - newCost) * ValveMap[valve].Flow
+		newFlow, newPath := OpenValves(newCost, newFlow, path, valve, newOpenValves)
+		if newFlow > bestFlow {
+			bestFlow = newFlow
+			bestPath = newPath
+		}
+	}
+	return bestFlow, bestPath
+}
+
+func OpenValves2(pathCost1, pathCost2, pathFlow int, path1, path2 []string, pos1, pos2 string, openValves map[string]bool) (int, []string, []string) {
+	bestFlow := pathFlow
+	path1 = append(path1, pos1)
+	path2 = append(path2, pos2)
+	bestPath1 := path1
+	bestPath2 := path2
+	for valve1, cost1 := range FlowGraph[pos1] {
+		if cost1+pathCost1 > 26 || openValves[valve1] {
+			continue
+		}
+		newCost1 := pathCost1
+		newCost1 += cost1
+
+		for valve2, cost2 := range FlowGraph[pos2] {
+			if cost2+pathCost2 > 30 || openValves[valve2] || valve1 == valve2 {
+				continue
+			}
+
+			newOpenValves := make(map[string]bool)
+			for k, v := range openValves {
+				newOpenValves[k] = v
+			}
+			newOpenValves[valve1] = true
+			newOpenValves[valve2] = true
+			newCost2 := pathCost2
+			newCost2 += cost2
+			//fmt.Printf("H:%v@%v; E:%v@%v\n", valve1, newCost1, valve2, newCost2)
+			newFlow := pathFlow
+			newFlow += (30 - newCost1) * ValveMap[valve1].Flow
+			newFlow += (30 - newCost2) * ValveMap[valve2].Flow
+			newFlow, newPath1, newPath2 := OpenValves2(newCost1, newCost2, newFlow, path1, path2, valve1, valve2, newOpenValves)
+			if newFlow > bestFlow {
+				bestFlow = newFlow
+				bestPath1 = newPath1
+				bestPath2 = newPath2
+			}
+		}
+	}
+	return bestFlow, bestPath1, bestPath2
 }
 
 func main() {
+	ValveMap = make(map[string]*Valve)
+	ValveGraph = make(dijkstra.Graph)
+	FlowGraph = make(dijkstra.Graph)
 	valves := aocutils.Lines(os.Args[1], ParseValve)
-	valveMap := make(map[string]*Valve)
-	for i := range valves {
-		valve := &valves[i]
-		fmt.Printf("Valve: %v\n", *valve)
-		valveMap[valve.Name] = valve
+
+	openValves := make(map[string]bool)
+	for _, v1 := range valves {
+		openValves[v1.Name] = false
+		if v1.Flow == 0 && v1.Name != "AA" {
+			continue
+		}
+		FlowGraph[v1.Name] = make(map[string]int)
+		for _, v2 := range valves {
+			if v2.Flow == 0 || v2.Name == v1.Name {
+				continue
+			}
+			_, cost, _ := ValveGraph.Path(v1.Name, v2.Name)
+			//fmt.Printf("Path from %v to %v @ %v: %v\n", v1.Name, v2.Name, cost, path)
+			FlowGraph[v1.Name][v2.Name] = cost + 1 // Include cost of opening the valve
+		}
 	}
-	fmt.Printf("Best pressure: %v\n", IDDFS(valveMap["AA"], &valveMap))
+
+	// How many valves can we get to in 30 minutes?
+	flow, path := OpenValves(0, 0, []string{}, "AA", openValves)
+	fmt.Printf("Maximum flow, part 1: %v (%v)\n", flow, path)
+
+	//flow, path1, path2 := OpenValves2(0, 0, 0, []string{}, []string{}, "AA", "AA", make(map[string]bool))
+	//fmt.Printf("Maximum flow, part 2: %v (%v) + (%v)\n", flow, path1, path2)
 }
